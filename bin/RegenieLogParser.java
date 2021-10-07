41ea9247927dd439fa91a665a85a507aecbb01a6
seppinho <sebastian.schoenherr@i-med.ac.at>
2021-10-07 15:54:20 +0200

    Parsing regenie options

--- bin/RegenieLogParser.java
+++ bin/RegenieLogParser.java
@@ -1,5 +1,3 @@
-
-
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //REPOS jcenter,jfrog-genepi-maven=https://genepi.jfrog.io/artifactory/maven/
 //DEPS info.picocli:picocli:4.6.1
@@ -10,134 +8,163 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
+import org.apache.poi.util.SystemOutLogger;
+
 import genepi.io.table.writer.CsvTableWriter;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+
 public class RegenieLogParser implements Callable<Integer> {
 
-    @Parameters(description = "Regenie log file")
-    private List<String> files;
-    @Option(names = "--output", description = "Output file ", required = true)
-    private String output;
+	private static String REGENIE_CALL_PATTERN = "Options in effect:";
 
-    public void setOutput(String output) {
-        this.output = output;
-    }
+	@Parameters(description = "Regenie log file")
+	private List<String> files;
+	@Option(names = "--output", description = "Output file ", required = true)
+	private String output;
 
-    public Integer call() throws Exception {
+	public void setOutput(String output) {
+		this.output = output;
+	}
 
-        assert (files != null);
-        assert (output != null);
+	public Integer call() throws Exception {
 
-        CsvTableWriter writer = new CsvTableWriter(new File(output).getAbsolutePath(), '\t', false);
+		assert (files != null);
+		assert (output != null);
 
-        String[] columnsWrite = { "Name", "Value" };
-        writer.setColumns(columnsWrite);
-        int variants = 0;
-        StringBuilder warningMsgs = new StringBuilder();
-        int count = 0;
-        for (String file : files) {
+		CsvTableWriter writer = new CsvTableWriter(new File(output).getAbsolutePath(), '\t', false);
 
-            Scanner s = new Scanner(new File(file));
-            while (s.hasNextLine()) {
+		String[] columnsWrite = { "Name", "Value" };
+		writer.setColumns(columnsWrite);
+		int variants = 0;
+		StringBuilder warningMsgs = new StringBuilder();
+		int count = 0;
+		boolean isRegenieOption = false;
+		StringBuilder optionsInEffect = new StringBuilder();
 
-                String line = s.nextLine();
+		for (String file : files) {
 
-                if (line.contains("-summary : bgen file")) {
-                    String value = line.split("\\s+")[14].trim();
-                    variants += Integer.valueOf(value);
-                } else if (line.contains("n_snps") && line.contains("pvar")) {
-                    String value = line.split("=")[1].trim();
-                    variants += Integer.valueOf(value);
-                } else if (line.contains("WARNING:")) {
-                    warningMsgs.append(line + "\n");
-                }
+			Scanner s = new Scanner(new File(file));
+			while (s.hasNextLine()) {
 
-                if (count > 0) {
-                    continue;
-                } else if (line.contains("REGENIE v")) {
-                    String value = line.split("\\s+")[3].trim();
-                    writer.setString(0, "Regenie Version ");
-                    writer.setString(1, value);
-                    writer.next();
-                } else if (line.contains("n_snps") && line.contains("bim")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Variants total (*.bim)");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("+number of variants remaining in the analysis")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Variants used (*.bim)");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("+number of genotyped individuals to keep")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Samples total (*.fam)");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("* phenotypes")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Number of defined phenotypes");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("-number of phenotyped individuals")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Phenotyped individuals total");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("number of individuals used in analysis")) {
-                    String value = line.split("=")[1].trim();
-                    writer.setString(0, "Phenotyped individuals used");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("--minMAC")) {
-                    String value = line.split("\\s+")[2].trim();
-                    writer.setString(0, "MAC limit");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                } else if (line.contains("--minINFO") && !line.contains("is skipped")) {
-                    String value = line.split("\\s+")[2].trim();
-                    writer.setString(0, "Imputation info score limit");
-                    writer.setDouble(1, Double.valueOf(value));
-                    writer.next();
-                } else if (line.contains("Number of ignored SNPs due to low MAC or info score")) {
-                    String value = line.split(":")[1].trim();
-                    writer.setString(0, "Variants ignored (low MAC or low info score)");
-                    writer.setInteger(1, Integer.valueOf(value));
-                    writer.next();
-                }
-            }
-            count++;
-            s.close();
-        }
+				String line = s.nextLine();
 
-        if (variants > 0) {
-            writer.setString(0, "Variants used (\\*.bgen or \\*.pvar)");
-            writer.setInteger(1, Integer.valueOf(variants));
-            writer.next();
-        }
+				// identify regenie options and skip line
+				if (line.contains(REGENIE_CALL_PATTERN)) {
+					isRegenieOption = true;
+					continue;
+				}
 
-        if (files.size() > 1) {
-            writer.setString(0, "Number of of parsed log files");
-            writer.setInteger(1, files.size());
-            writer.next();
-        }
+				// parse all lines with regenie option included
+				if (isRegenieOption) {
+					if (!line.endsWith("\\")) {
+						isRegenieOption = false;
+					} else {
+						optionsInEffect.append(line.substring(0, line.indexOf('\\')));
+					}
+				}
 
-        if (warningMsgs.length() > 0) {
-            writer.setString(0, "Warnings");
-            writer.setString(1, warningMsgs.toString());
-            writer.next();
-        }
+				if (line.contains("-summary : bgen file")) {
+					String value = line.split("\\s+")[14].trim();
+					variants += Integer.valueOf(value);
+				} else if (line.contains("n_snps") && line.contains("pvar")) {
+					String value = line.split("=")[1].trim();
+					variants += Integer.valueOf(value);
+				} else if (line.contains("WARNING:")) {
+					warningMsgs.append(line + "\n");
+				}
 
-        writer.close();
-        return 0;
-    }
+				// Several log files produced (step 2), therefore skip everything from here.
+				if (count > 0) {
+					continue;
+				} else if (line.contains("REGENIE v")) {
+					String value = line.split("\\s+")[3].trim();
+					writer.setString(0, "Regenie Version ");
+					writer.setString(1, value);
+					writer.next();
+				} else if (line.contains("n_snps") && line.contains("bim")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Variants total (*.bim)");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("+number of variants remaining in the analysis")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Variants used (*.bim)");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("+number of genotyped individuals to keep")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Samples total (*.fam)");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("* phenotypes")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Number of defined phenotypes");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("-number of phenotyped individuals")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Phenotyped individuals total");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("number of individuals used in analysis")) {
+					String value = line.split("=")[1].trim();
+					writer.setString(0, "Phenotyped individuals used");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("--minMAC")) {
+					String value = line.split("\\s+")[2].trim();
+					writer.setString(0, "MAC limit");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				} else if (line.contains("--minINFO") && !line.contains("is skipped")) {
+					String value = line.split("\\s+")[2].trim();
+					writer.setString(0, "Imputation info score limit");
+					writer.setDouble(1, Double.valueOf(value));
+					writer.next();
+				} else if (line.contains("Number of ignored SNPs due to low MAC or info score")) {
+					String value = line.split(":")[1].trim();
+					writer.setString(0, "Variants ignored (low MAC or low info score)");
+					writer.setInteger(1, Integer.valueOf(value));
+					writer.next();
+				}
+			}
+			count++;
+			s.close();
+		}
 
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new RegenieLogParser()).execute(args);
-        System.exit(exitCode);
-    }
+		if (variants > 0) {
+			writer.setString(0, "Variants used (\\*.bgen or \\*.pvar)");
+			writer.setInteger(1, Integer.valueOf(variants));
+			writer.next();
+		}
+
+		if (files.size() > 1) {
+			writer.setString(0, "Number of of parsed log files");
+			writer.setInteger(1, files.size());
+			writer.next();
+		}
+
+		if (warningMsgs.length() > 0) {
+			writer.setString(0, "Warnings");
+			writer.setString(1, warningMsgs.toString());
+			writer.next();
+		}
+
+		writer.setString(0, "Regenie Call");
+		writer.setString(1, optionsInEffect.toString());
+		writer.next();
+
+		writer.close();
+
+		return 0;
+	}
+
+	public static void main(String... args) {
+		int exitCode = new CommandLine(new RegenieLogParser()).execute(args);
+		System.exit(exitCode);
+	}
 
 }
