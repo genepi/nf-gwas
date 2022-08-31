a7c1b59ef5717452798cf308993f344bfe0f7e6b
seppinho <sebastian.schoenherr@i-med.ac.at>
2022-08-31 12:19:57 +0200

    Integrate Micheles and Johannas improvements

--- reports/functions.R
+++ reports/functions.R
@@ -4,29 +4,30 @@ manhattan_prep <- function(df) {
     # Compute chromosome size
     group_by(CHROM) %>%
     summarise(CHROM_len=max(GENPOS)) %>%
-    
+
     # Calculate cumulative start position of each chromosome
     mutate(tot=cumsum(as.numeric(CHROM_len))-CHROM_len) %>%
     select(-CHROM_len) %>%
-    
+
     # Add this info to the initial dataset
-    left_join(regenieTable_annotated, ., by=c("CHROM"="CHROM")) %>%
-    
+    left_join(df, ., by=c("CHROM"="CHROM")) %>%
+
     # Add a cumulative position of each SNP
     arrange(CHROM, GENPOS) %>%
     mutate( GENPOScum=GENPOS+tot)
   return(df_prep)
-    
+
 }
 
 ## Create Manhattan plot ===========================
 #sources: https://www.r-graph-gallery.com/101_Manhattan_plot.html with modifications
 
-manhattan_plot <- function(df_prep, 
-                           ymax = 0) {
+manhattan_plot <- function(df_prep,
+                           ymax = 0,
+                           hlines = TRUE) {
   # prepare x axis
-  axisdf <-  df_prep %>% 
-    group_by(CHROM) %>% 
+  axisdf <-  df_prep %>%
+    group_by(CHROM) %>%
     summarize(center=( max(GENPOScum) + min(GENPOScum) ) / 2 )
   # prepare y axis
   if (ymax == 0) {
@@ -34,14 +35,14 @@ manhattan_plot <- function(df_prep,
   } else {
     ydim <- c(0, ymax)
   }
-  
+
   # create plot
   plot <- ggplot(df_prep, aes(x=GENPOScum, y=LOG10P)) +
-    
+
     # Show all points
     geom_point(aes(color=as.factor(CHROM))) +
     scale_color_manual(values = rep(c("#779ECB", "#03254c"), 22 )) +
-    
+
     # custom X axis:
     scale_x_continuous(label = axisdf$CHROM,
                        breaks= axisdf$center,
@@ -50,15 +51,8 @@ manhattan_plot <- function(df_prep,
                        guide = guide_axis(check.overlap = TRUE)) +
     # custom y step 1annotation_limit
     scale_y_continuous(expand = c(0, 0),
-                       name=expression(-log[10](italic(p))),
+                       name=expression(-log[10](italic(P))),
                        limits = ydim) +
-    geom_hline(yintercept = -log10(5e-08),
-               linetype ="longdash",
-               color ="firebrick") + # genomewide significance
-    geom_hline(yintercept = -log10(1e-5),
-               linetype ="longdash",
-               color ="darkgrey")  + # suggestive significance
-    
     # Custom the theme:
     theme_classic() +
     theme(
@@ -69,5 +63,17 @@ manhattan_plot <- function(df_prep,
       axis.title = element_text(size = 14),
       axis.ticks = element_line(color = "black")
     )
+  # switch hlines off
+  if (hlines == TRUE) {
+    plot <- plot +
+      geom_hline(yintercept = -log10(5e-08),
+                 linetype ="longdash",
+                 color ="firebrick") + # genomewide significance
+      geom_hline(yintercept = -log10(1e-5),
+                 linetype ="longdash",
+                 color ="darkgrey")  # suggestive significance
+  } else {
+    plot <- plot
+  }
   return(plot)
 }
